8d71a896132b2d42c11aa770e555c2c9fa1660e3
ayotnom <amontoy6@u.rochester.edu>
2025-05-22 00:22:24 -0700

    amplify cloud env setup

---
+++ amplify/storage/resource.ts
@@ -0,0 +1,10 @@
+mport { defineStorage } from "@aws-amplify/backend";
+
+export const storage = defineStorage({
+	  name: "amplifyNotesDrive",
+	  access: (allow) => ({
+		  "media/{entity_id}/*": [
+			  allow.entity("identity").to(["read", "write", "delete"]),
+		  ],
+		}),
+});
