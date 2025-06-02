0696ea022873e75bd232bd3c109a76847e840b97
ayotnom <amontoy6@u.rochester.edu>
2025-06-02 11:43:01 -0700

    heading level v2

--- src/App.tsx
+++ src/App.tsx
@@ -172,7 +172,7 @@ export default function App() {
                 className="box"
               >
                 <View>
-                  <Heading level="h3">{note.name}</Heading>
+                  <Heading level={'h3' as const}>{note.name}</Heading>
                 </View>
                 <Text fontStyle="italic">{note.description}</Text>
                 {note.image && (
